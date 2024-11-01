894c9d8c29c93c5c8989397d5baf38ff57f08d43
Pat Nadolny <patnadolny@gmail.com>
2024-10-31 21:08:00 -0400

    cookie cutter

---
+++ tap_intacct/__main__.py
@@ -0,0 +1,7 @@
+"""Intacct entry point."""
+
+from __future__ import annotations
+
+from tap_intacct.tap import TapIntacct
+
+TapIntacct.cli()
---
+++ tests/test_core.py
@@ -0,0 +1,22 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import datetime
+
+from singer_sdk.testing import get_tap_test_class
+
+from tap_intacct.tap import TapIntacct
+
+SAMPLE_CONFIG = {
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    # TODO: Initialize minimal tap config
+}
+
+
+# Run standard built-in tap tests from the SDK:
+TestTapIntacct = get_tap_test_class(
+    tap_class=TapIntacct,
+    config=SAMPLE_CONFIG,
+)
+
+
+# TODO: Create additional tests as appropriate for your tap.
